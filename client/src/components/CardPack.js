32beb9e53fd56250b748f4f8d04fd04ba9dacb60
mreilly4 <mreilly4@stevens.edu>
2021-12-13 17:07:35 -0500

    Rarities for isHolo and Popularity added to CardPack.js, as well as both necessary useQuery()

--- client/src/components/CardPack.js
+++ client/src/components/CardPack.js
@@ -13,17 +13,73 @@ const useStyles = makeStyles({
 const CardPack = () => {
     const classes = useStyles();
 	const [ cardData, setCardData ] = useState(undefined);
+    const { loading, error, popularData } = useQuery(
+        queries.GET_POPULAR,
+        {
+          fetchPolicy: 'cache-and-network'
+        }
+      );
+    const { load, err, pokemonData } = useQuery(
+        queries.GET_ALL_POKEMON,
+        {
+            fetchPolicy: 'cache-and-network'
+        }
+    );
 	let card = null;
 
     useEffect(
         () => {
             const fetchData = async () =>{
                 try{
-                    let result = [0, 0, 0];
-                    const ids = [Math.floor(Math.random(10220))+1, Math.floor(Math.random(10220)+1, Math.floor(Math.random(10220)+1))];
+                    let result=new Array();
+                    let popular=0;
+                    let popularNum = Math.floor(Math.random(100))+1;
+                    if(popularNum==1){
+                        popular=3;
+                    }
+                    else if(popularNum>=2 && popularNum<=4){
+                        popular=2;
+                    }
+                    else if(popularNum>=5 && popularNum<=10){
+                        popular=1;
+                    }
+                    else{
+                        popular=0;
+                    }
+                    let popularIds=new Array();
+                    for(let poke in popularData){
+                        popularIds.push(poke.id);
+                    }
+                    let ids=new Array();
                     for(let i=0; i<3; i++){
-                        // Call cards with useQuery for the id values in the ids array
-                        // and push each of these into result
+                        if(popular>0){
+                            let random=Math.floor(Math.random(popularIds.length));
+                            ids.push(popularIds[random]);
+                            popular--;
+                        }
+                        else{
+                            random: while(popular==0){
+                                let rand=Math.floor(Math.random(898))+1;
+                                if(popularIds.includes(rand)){
+                                    continue random;
+                                }
+                                else{
+                                    ids.push(rand);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    for(let k=0; k<3; k++){
+                        const pokemon=pokemonData.find(p => p.id==ids[k]);
+                        const holo=Math.floor(Math.random(100));
+                        if(holo<=1){
+                            pokemon.isHolo=true;
+                        }
+                        else{
+                            pokemon.isHolo=false;
+                        }
+                        result.push(pokemon);
                     }
                     console.log(result);
                     setCardData(result);
