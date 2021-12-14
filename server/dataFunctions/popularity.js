84988ba2ea00d3439a6375b71bdc6029058adf13
jleeson306 <jleeson@stevens.edu>
2021-12-14 16:22:53 -0500

    Added pokemonScore to popularity

--- server/dataFunctions/popularity.js
+++ server/dataFunctions/popularity.js
@@ -35,7 +35,7 @@ const getPokemonPopularity = async function(pokemonName) {
 	const pokemon = await popularityCollection.findOne({pokeName: pokemonName});
 
 	if(pokemon == null) {       //if the pokemon does not exist for us yet, I am creating it and starting it at a value of 500
-        let newPokemon = {pokeName: pokemonName, pokePop: 500};
+        let newPokemon = {pokeName: pokemonName, pokePop: 500, pokeWins: 0, pokeLosses: 0};
         const inIn = await popularityCollection.insertOne(newPokemon);
         if(inIn.insertCount === 0) {
             throw({code: 500, message: "getPokemonPopularity: pokemon is not in database, and could not add it"});
@@ -69,7 +69,7 @@ const changePokemonPopularity = async function(pokemonName, toChange) {
 	if(pokemon == null) {       //if the pokemon does not exist for us yet, I am creating it and starting it at a value of 500
         if((500 + toChange) <= 0) {finalPop = 1;}
         else{finalPop += 500;}
-        let newPokemon = {pokeName: pokemonName, pokePop: finalPop};
+        let newPokemon = {pokeName: pokemonName, pokePop: finalPop, pokeWins: 0, pokeLosses: 0};
         const inIn = await popularityCollection.insertOne(newPokemon);
         if(inIn.insertCount === 0) {
             throw({code: 500, message: "changePokemonPopularity: pokemon is not in database, and could not add it"});
@@ -89,6 +89,54 @@ const changePokemonPopularity = async function(pokemonName, toChange) {
         }
         else{
             return finalPop;
+        }
+	}
+}
+
+const changePokemonScore = async function(pokemonName, didWin) {
+    if(arguments.length != 2 || pokemonName == undefined) {
+		throw({code: 400, message: "changePokemonScore: you are missing pokemonName or didWin"});
+	}
+	if(typeof userName !== 'string' || userName.trim() == "") {
+		throw({code: 400, message: "changePokemonScore: pokemonName must be a string that isn't empty or just spaces"});
+	}
+    if(typeof didWin !== 'bool') {
+		throw({code: 400, message: "changePokemonScore: didWin must be a boolean"});
+	}
+
+    const popularityCollection = await popularity();
+
+	const pokemon = await popularityCollection.findOne({pokeName: pokemonName});
+
+	if(pokemon == null) {       //if the pokemon does not exist for us yet, I am creating it and starting it at a value of 500
+        let newPokemon;
+        if(didWin) {
+            newPokemon = {pokeName: pokemonName, pokePop: 500, pokeWins: 1, pokeLosses: 0};
+        }
+        else {
+            newPokemon = {pokeName: pokemonName, pokePop: 500, pokeWins: 0, pokeLosses: 1};
+        }
+        const inIn = await popularityCollection.insertOne(newPokemon);
+        if(inIn.insertCount === 0) {
+            throw({code: 500, message: "changePokemonScore: pokemon is not in database, and could not add it"});
+        }
+        else{
+            return didWin;
+        }
+	}
+    else{
+        let upin
+        if(didWin) {
+            upin = await popularityCollection.updateOne({pokeName: pokemonName}, {$set: {pokeWins: (pokemon.pokeWins + 1)}});
+        }
+        else {
+            upin = await popularityCollection.updateOne({pokeName: pokemonName}, {$set: {pokeLosses: (pokemon.pokeLosses + 1)}});
+        }
+        if (upin === 0) {
+            throw({code: 500, message: "changePokemonScore: failed to update"});
+        }
+        else{
+            return didWin;
         }
 	}
 }
