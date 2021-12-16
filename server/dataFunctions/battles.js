4f03c5700068b49f95611a003a50fd6f8542a526
jleeson306 <jleeson@stevens.edu>
2021-12-15 19:17:35 -0500

    Bets are handled, should all be serverside good

--- server/dataFunctions/battles.js
+++ server/dataFunctions/battles.js
@@ -1,8 +1,11 @@
 const mongoCollections = require('../config/mongoCollections');
 const battles = mongoCollections.battleCollection;
+const users = mongoCollections.usersCollection;
 const {ObjectId} = require('mongodb');
 const { battleCollection } = require('../config/mongoCollections');
+const { usersCollection } = require('../config/mongoCollections');
 const popularityData = require('./popularity');
+const userData = require('./user');
 const timePerBattle = 60000;//in milliseconds 
 
 const removeAll = async function() {
@@ -59,8 +62,10 @@ const createBattle = async function(trainerOne, trainerTwo, pokemonOne, pokemonT
 		throw({code: 500, message: "createBattle: unable to add that battle"});
 	}
 	else{
-        let ret = await inIn.insertedId.toString();
-		return ret;
+        let newBattleID = await inIn.insertedId.toString();
+        newBattle._id = newBattleID;
+        newBattle.winner = "To Be Determined";
+		return newBattle;
 	}
 }
 
@@ -85,15 +90,10 @@ const getBattle = async function(battleID) {
         }
         else{
             const nowDate = new Date().getTime();
-            if(nowDate < battle.timeStamp){
-                let timeToWait = battle.timeStamp - nowDate;
-                timeToWait = timeToWait / 1000;
+            if(nowDate <= battle.timeStamp){
                 battle.winner = "To Be Determined";
-                return({code: 200, battleInfo: battle, timeTillResult: timeToWait});
             }
-            else{
-                return({code: 200, battleInfo: battle, timeTillResult: 0});
-            }
+            return battle;
         }
     }
     else {
@@ -101,8 +101,100 @@ const getBattle = async function(battleID) {
     }
 }
 
+const getCurrentBattles = async function() {
+    const battleCollection = await battles();
+    const nowDate = new Date().getTime();
+    const battles = await battleCollection.find({timeStamp: {$gt: nowDate} }).toArray();
+
+    battles.forEach(element => {
+        element.winner = "To Be Determined";
+    });
+
+    return battles;
+}
+
+const createBet = async function(userName, betAmount, battleID, predectedWinner) {
+	if(arguments.length != 4 || battleID == undefined || userName == undefined || betAmount == undefined || predectedWinner == undefined) {
+		throw({code: 400, message: "createBet: you are missing one of userName, betAmount, predectedWinner, or battleID"});
+	}
+    if(typeof userName !== 'string' || userName.trim() == "") {
+		throw({code: 400, message: "createBet: userName must be a string that isn't empty or just spaces"});
+	}
+    if(typeof predectedWinner !== 'string' || userName.trim() == "") {
+		throw({code: 400, message: "createBet: predectedWinner must be a string that isn't empty or just spaces"});
+	}
+	if(typeof betAmount !== 'number' || betAmount == 1) {
+		throw({code: 400, message: "createBet: betAmount must be a number that isn't zero"});
+	}
+	else if (typeof battleID === 'string') {
+		battleID = ObjectId(battleID);
+	}
+	else {
+		throw({code: 400, message: "createBet: battleID must be a string'"});
+	}
+
+    if(battleID instanceof ObjectId) {
+        const usersCollection = await users();
+        const user = await usersCollection.findOne({userName: userName});
+        if(user == null) {
+            throw({code: 404, message: "createBet: a user with that display name does not exist"});
+        }
+        const battleCollection = await battles();
+        const battle = await battleCollection.findOne({_id: battleID});
+        if(battle == null) {
+            throw({code: 404, message: "createBet: a battle with that ID does not exist"});
+        }
+
+        if(user.wallet < betAmount) {
+            betAmount = user.wallet;
+        }
+        await userData.changeFunds(userName, (betAmount * -1))
+
+        newBet = {
+            userName: userName,
+            predectedWinner: predectedWinner,
+            payout: (betAmount * 2)
+        }
+        const upin = await battleCollection.updateOne({_id: battleID}, {$push: {battleBets: newBet }});
+        if (upin === 0) {
+            throw({code: 500, message: "createBet: unable to add that bet to the database"});
+        }
+        else{
+            return({code: 200, message: "createBet: bet successfully added"});
+        } 
+    }
+    else {
+        throw({code: 400, message: "createBet: battleID must be a valid ObjectID'"});
+    }
+}
+
+const payoutAllBattles = async function() {
+    const battleCollection = await battles();
+    const nowDate = new Date().getTime();
+    const battles = await battleCollection.find({timeStamp: {$lt: nowDate} }).toArray();
+
+    battles.forEach(element => {
+        if(element.payoutGiven) {
+            await battleCollection.deleteOne({_id: element._id});
+        }
+        else{
+            element.battleBets.forEach(subElement => {
+                if(subElement.predectedWinner == element.winner) {
+                    await userData.changeFunds(subElement.userName, subElement.payout)
+                }
+            });
+            await battleCollection.updateOne({_id: element._id}, {$set: {payoutGiven: true}});
+        }
+    });
+
+    return "wow I hated writing this, even though it is so small";
+}
+
 module.exports = {
 	removeAll,
     createBattle,
-    getBattle
+    getBattle,
+    getCurrentBattles,
+    createBet,
+    payoutAllBattles
 }
