7af29a3953015e1a7d2da7ce5ac2f5b76393cf78
mreilly4 <mreilly4@stevens.edu>
2021-12-18 01:56:47 -0500

    Almost fixed all the logic in CardPack and Battle

--- client/src/components/CardPack.js
+++ client/src/components/CardPack.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PokeCard from './PokeCard';
-import { useQuery, useMutation, useApolloClient } from '@apollo/client';
+import { useQuery, useMutation, ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { Grid, makeStyles } from '@material-ui/core';
 import queries from '../queries';
 import mutations from '../mutations';
@@ -16,25 +16,67 @@ const useStyles = makeStyles({
 const CardPack = () => {
   const classes = useStyles();
   const [ cardData, setCardData ] = useState(undefined);
-  const client = useApolloClient();
-  const addPokemon=useMutation(mutations.ADD_POKEMON);
+  const [ loading, setLoading ] = useState(false);
+  /*const client = new ApolloClient({
+    link: new HttpLink({
+      uri: "http://localhost:4000/"
+    }),
+    cache: new InMemoryCache()
+  });*/
+
+  /*let poke=new Array();
+
+  const getPokemonName = async (r) => {
+    const pokemon=await axios.get(`https://pokeapi.co/api/v2/pokemon/${r}/`);
+    poke.push(pokemon);
+    return pokemon.data.name;
+  }
+
+  const randomOne=Math.floor(Math.random()*898)+1;
+  const randomTwo=Math.floor(Math.random()*898)+1;
+  const randomThree=Math.floor(Math.random()*898)+1;*/
+
+  /*const { popularityOne } = useQuery(queries.GET_POPULARITY, {
+    variables: { pokemonName: getPokemonName(randomOne)}
+  });
+  const { popularityTwo } = useQuery(queries.GET_POPULARITY, {
+    variables: { pokemonName: getPokemonName(randomTwo)}
+  });
+  const { popularityThree } = useQuery(queries.GET_POPULARITY, {
+    variables: { pokemonName: getPokemonName(randomThree)}
+  });*/
+
+  /*if(popularityOne && popularityTwo && popularityThree){
+  for(let i=0; i<3; i++){
+    const shiny=Math.floor(Math.random()*100)+1;
+    if(shiny<=2){
+      poke[i].isShiny=true;
+    }
+    else{
+      poke[i].isShiny=false;
+    }
+  }
+} */
+  const [addPokemon, {data, load, error}]=useMutation(mutations.ADD_POKEMON);
   let card=null;
 
-  async function popularQuery(pokemon){
-    const { loading, error, popularData } = await client.query({query: queries.GET_POPULARITY, variables: { pokemonName: pokemon.data.name}});
-    if(error){
-      return `${error.message}`
-    }
+  /*async function popularQuery(pokemon){
+    console.log(pokemon)
+    const { popularData } = await client.query({query: queries.GET_POPULARITY, variables: { pokemonName: pokemon.name}});
+    console.log(popularData)
     return popularData;
-  }
+  }*/
 
   useEffect(
     () => {
+      console.log("useEffect fired")
       const fetchData = async () =>{
+        console.log("fetchData fired")
         try{
+          setLoading(true);
           let result=new Array();
-          let popular=0;
-          let popularNum=Math.floor(Math.random(100))+1;
+          /*let popular=0;
+          let popularNum=Math.floor(Math.random()*100)+1;
           if(popularNum==1){
             popular=3;
           }
@@ -46,50 +88,48 @@ const CardPack = () => {
           }
           else{
             popular=0;
-          }
+          }*/
           for(let i=0; i<3; i++){
-            if(popular>0){
+            /*if(popular>0){
               console.log(popular);
-            pop: while(popular>0){
-              const random=Math.floor(Math.random(898))+1;
+            pop: while(popular>0){*/
+              const random=Math.floor(Math.random()*898)+1;
+              console.log(random)
               const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${random}/`);
-              const popularity=popularQuery(pokemon);
-              if(typeof popularity != 'number'){
-                popular=-1;
-                throw `${popularity}`
-              }
-              else if(popularity<750){
+              console.log(pokemon.data)
+              /*const popularity=await popularQuery(pokemon.data);
+              if(popularity<750){
                 continue pop;
               }
-              else{
-                const shiny=Math.floor(Math.random(100));
+              else{*/
+                const shiny=Math.floor(Math.random()*100);
                 if(shiny<=1){
+                  //popular--;
                   pokemon.data.isShiny=true;
                   result.push(pokemon.data);
                 }
                 else{
+                  //popular--;
                   pokemon.data.isShiny=false;
                   result.push(pokemon.data);
                 }
-                break;
-              }
-            }
+              //}
+          /*  }
           }
           else{
             random: while(popular==0){
-              const random=Math.floor(Math.random(898))+1;
+              const random=Math.floor(Math.random()*898)+1;
+              console.log(random);
               const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${random}/`);
-              const popularity=popularQuery(pokemon);
-              if(typeof popularity != 'number'){
-                popular=-1;
-                throw `${popularity}`;
-              }
-              else if(popularity>=750){
+              console.log(pokemon)
+              const popularity=await popularQuery(pokemon);
+              console.log(popularity)
+              if(popularity>=750){
                 continue random;
               }
               else{
-                const shiny=Math.floor(Math.random(100));
-                if(shiny<=1){
+                const shiny=Math.floor(Math.random()*100)+1;
+                if(shiny<=2){
                   pokemon.data.isShiny=true;
                   result.push(pokemon.data);
                 }
@@ -104,22 +144,29 @@ const CardPack = () => {
           }
           if(popular==-1){
             return <h2>Failed to Fetch</h2>
-          }
+          }*/
+        }
           console.log(result);
           setCardData(result);
+          setLoading(false);
         }
         catch(e){
           console.log(e)
         }
       }
-      fetchData()
+      if(!loading){
+      fetchData();
+      }
     },
     []
   )
   
   const CardGrid = (pokemon) => {
+    console.log("cardgrid")
     const id=pokemon.id;
+    pokemon.pokemonID=id;
     const name=pokemon.name;
+    pokemon.pokemonName=name;
     const image=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
     const shiny=pokemon.isShiny;
     const userName="firebase" // Will be received from firebase
@@ -131,18 +178,24 @@ const CardPack = () => {
     )
   }
 
-  const carder = () => {
-    card =
-      cardData.map((pokemon) => {
-        return CardGrid(pokemon);
-      })
+  console.log(cardData)
+
+  if(cardData){
+
+  card =
+    cardData &&
+    cardData.map((pokemon) => {
+      return CardGrid(pokemon);
+    })
+
+  console.log(card)
   }
 
   return(
     <div>
       <h1>Get a new Card Pack!</h1>
       <br />
-      <button onClick={carder}>Claim Card Pack!</button>
+      <button onClick="">Claim Card Pack!</button>
       <Grid container className={classes.grid} spacing={5}>
         {card}
       </Grid>
