3a7f6fd98e2dea435d11bcee42c6de82282022f1
nrfq <43225935+nrfq@users.noreply.github.com>
2021-12-18 20:52:38 -0500

    voting styling improvements

--- client/src/components/Survey.js
+++ client/src/components/Survey.js
@@ -1,100 +1,118 @@
 import React, { useState, useEffect } from "react";
 import PokeCard from './PokeCard';
 import { useMutation, useQuery } from '@apollo/client';
-import { Grid, makeStyles } from '@material-ui/core';
-import { Modal, Spinner } from 'react-bootstrap';
+import { Button, Container, Row, Col, Modal, Spinner } from 'react-bootstrap';
 import axios from 'axios';
 import mutations from '../mutations';
 
-const useStyles = makeStyles({
-  grid: {
-    flexGrow: 1,
-    flexDirection: 'row'
-  }
-});
-
 const Survey = () => {
-  const classes = useStyles();
   const [ cardData, setCardData ] = useState(undefined);
   const [loading, setLoading] = useState(true);
+  const [ error, setError ] = useState(null);
+  const [ modal, setModal ] = useState({show: false, title: "", message: "", func: null});
   const [changePopularity, {popResults}] = useMutation(mutations.CHANGE_POPULARITY);
   const [changeFunds, {fundResults}] = useMutation(mutations.CHANGE_FUNDS);
   let card = null;
 
+  const closeModal = () => {
+    setModal({...modal, show: false});
+  }
+
+  const fetchData = async () => {
+    setLoading(true);
+    setModal({...modal, show: false});
+    try{
+      let result = [];
+      let randOne = Math.floor(Math.random() * 898) + 1
+      let pokemonOne = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randOne}/`);
+      let randTwo = Math.floor(Math.random() * 898) + 1
+      let pokemonTwo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randTwo}/`);
+      result.push(pokemonOne);
+      result.push(pokemonTwo);
+      setCardData(result);
+    }
+    catch(e){
+      console.log(e)
+    }
+    setLoading(false);
+  } 
+
   useEffect(
     () => {
-      const fetchData = async () =>{
-        try{
-          let result=new Array();
-          let randOne = Math.floor(Math.random() * 898) + 1
-          let pokemonOne = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randOne}/`);
-          let randTwo = Math.floor(Math.random() * 898) + 1
-          let pokemonTwo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randTwo}/`);
-          result.push(pokemonOne);
-          result.push(pokemonTwo);
-          setCardData(result);
-          setLoading(false);
-        }
-        catch(e){
-          console.log(e)
-        }
-      }
       fetchData()
     },
     []
   )
+
   if (loading) {
     return (
-      <Spinner animation="border" style={{alignSelf: 'center'}}/>
+      <Spinner animation="border" />
     );
   }
-  else {
-    
-    const ourUsersCards = (pokemon) => {
-      changePopularity({
-        variables: {pokemonName: pokemon.pokemonName, toChange: 5}
-      });
-      changeFunds({
-        variables: {userName: "James", toChange: 5} // swap "James" with name given from firebase
-      });
-      alert(`You voted for ${pokemon.pokemonName}! You just earned 5 PokeDollars`);
-      window.location.reload(false);
-    }
-    
-    const CardGrid = (pokemon) => {
-      let pokeCardData = {
-        pokemonID: pokemon.data.id,
-        pokemonName: pokemon.data.name,
-        imageLink: pokemon.data.sprites.front_default,
-        isShiny: false
-      };
-      return(
-        <Grid item xs={12} sm={6} md={4} lg={2} xl={2} key={pokeCardData.pokemonID}>
-          <PokeCard pokemon={pokeCardData}></PokeCard>
-          <button onClick={() => ourUsersCards(pokeCardData)}>I Like this Pokemon</button>
-        </Grid>
-      )
-    }
-
-    const carder = () => {
-      card =
-        cardData.map((pokemon) => {
-          return CardGrid(pokemon);
-        })
-    }
-
-    carder();
-
-    return(
-      <div>
-        <h1>Pick your favorite pokemon!</h1>
-        <br />
-        <Grid container className={classes.grid} spacing={5}>
-          {card}
-        </Grid>
-      </div>
+  if(error){
+    return (
+      <h2>Error</h2>
     )
   }
+  const ourUsersCards = (pokemon) => {
+    changePopularity({
+      variables: {pokemonName: pokemon.pokemonName, toChange: 5}
+    });
+    changeFunds({
+      variables: {userName: "James", toChange: 5} // swap "James" with name given from firebase
+    });
+    setModal({show:true, title: "Success", message: `You voted for ${pokemon.pokemonName}! You just earned 5 PokéDollars`, func: fetchData});
+  }
+
+
+    
+  const CardGrid = (pokemon) => {
+    let pokeCardData = {
+      pokemonID: pokemon.data.id,
+      pokemonName: pokemon.data.name,
+      imageLink: pokemon.data.sprites.front_default,
+      isShiny: false
+    };
+    return(
+      <Col style={{margiLeft: '1rem', marginRight: '1rem'}} xs={12} sm={6} md={4} lg={2} xl={2} key={pokeCardData.pokemonID}>
+        <button className="btn-vote" onClick={() => ourUsersCards(pokeCardData)}>
+          <PokeCard pokemon={pokeCardData}></PokeCard>
+        </button>
+      </Col>
+    )
+  }
+
+  card =
+    cardData.map((pokemon) => {
+      return CardGrid(pokemon);
+  })
+
+  return(
+    <div>
+      <h1>Pick your favorite Pokémon!</h1>
+      <Button className="btn-secondary" onClick={() => setModal({show: true, title: 'About this page', message: 
+      'By voting on a pokémon, you help fuel our ranking system. To award you for your help, you receive 5 free PokéDollars', 
+      func: closeModal})}>
+        ?
+      </Button>
+      <Container fluid>
+        <Row style={{paddingTop: '4rem'}} className="justify-content-center">
+          {card}
+        </Row>
+      </Container>
+      <Modal show={modal.show} onHide={modal.func}>
+        <Modal.Header closeButton>
+          <Modal.Title>{modal.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{modal.message}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={modal.func}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )
 }
 
 export default Survey;
